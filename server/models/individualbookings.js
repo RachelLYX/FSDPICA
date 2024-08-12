b655ee1c1207b689a65ae3688feb8696a4d1ff60
manigandan0307 <manigandan50300@gmail.com>
2024-08-13 01:16:14 +0800

    Create individualbookings.js

---
+++ server/models/individualbookings.js
@@ -0,0 +1,50 @@
+module.exports = (sequelize, DataTypes) => {
+    const IndividualBookings = sequelize.define("IndividualBookings", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        event_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        phone: {
+            type: DataTypes.STRING(20),
+            allowNull: true,
+        },
+        special_requirements: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        created_at: {
+            type: DataTypes.TIMESTAMP,
+            defaultValue: DataTypes.NOW,
+            allowNull: true,
+        },
+        event_name: {
+            type: DataTypes.STRING(45),
+            allowNull: true,
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        status: {
+            type: DataTypes.ENUM('pending', 'confirmed', 'rejected'),
+            defaultValue: 'pending',
+        }
+    }, {
+        tableName: 'individualbookings',
+        timestamps: false,
+    });
+    return IndividualBookings;
+}
