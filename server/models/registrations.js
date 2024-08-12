9363a194950208c4957134088576c879c39c9b62
manigandan0307 <manigandan50300@gmail.com>
2024-08-13 01:15:25 +0800

    Update registrations.js

--- server/models/registrations.js
+++ server/models/registrations.js
@@ -1,54 +1,46 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../database.js';
-import Events from './events.js'; // Assuming you have an Events model
+module.exports = (sequelize, DataTypes) => {
+    const Registrations = sequelize.define("Registrations", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        event_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        type: {
+            type: DataTypes.ENUM('group', 'individual'),
+            allowNull: true,
+        },
+        participants: {
+            type: DataTypes.INTEGER,
+            defaultValue: 1,
+        },
+        institution: {
+            type: DataTypes.STRING(255),
+            allowNull: true,
+        },
+        poc_name: {
+            type: DataTypes.STRING(255),
+            allowNull: true,
+        },
+        poc_contact: {
+            type: DataTypes.STRING(20),
+            allowNull: true,
+        },
+        poc_email: {
+            type: DataTypes.STRING(255),
+            allowNull: true,
+        },
+        special_requirements: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        }
+    }, {
+        tableName: 'registrations',
+        timestamps: false,
+    });
+    return Registrations;
+}
 
-const Registrations = sequelize.define('Registrations', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  event_id: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    references: {
-      model: Events,
-      key: 'id',
-    },
-    onDelete: 'CASCADE',
-    onUpdate: 'CASCADE',
-  },
-  type: {
-    type: DataTypes.ENUM('group', 'individual'),
-    allowNull: true,
-  },
-  participants: {
-    type: DataTypes.INTEGER,
-    defaultValue: 1,
-  },
-  institution: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  poc_name: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  poc_contact: {
-    type: DataTypes.STRING(20),
-    allowNull: true,
-  },
-  poc_email: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  special_requirements: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-}, {
-  tableName: 'registrations',
-  timestamps: false,
-});
-
-export default Registrations;
