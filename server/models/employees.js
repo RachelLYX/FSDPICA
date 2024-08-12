bdffe4542f069720e904884256a1a053bd82f419
manigandan0307 <manigandan50300@gmail.com>
2024-08-13 01:19:20 +0800

    Create employees.js

---
+++ server/models/employees.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+    const Employees = sequelize.define("Employees", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+            unique: true,
+        },
+        email: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+        birth_date: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        role: {
+            type: DataTypes.ENUM('admin', 'moderator'),
+            defaultValue: 'admin',
+        },
+        created_at: {
+            type: DataTypes.TIMESTAMP,
+            defaultValue: DataTypes.NOW,
+            allowNull: true,
+        }
+    }, {
+        tableName: 'employees',
+        timestamps: false,
+    });
+    return Employees;
+}
